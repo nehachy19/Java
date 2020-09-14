ebfda51b191aa78f722d0d31130dadd5f8b23ecc
Neha Vats <nehavats2020@gmail.com>
2020-09-14 12:24:49 -0400

    Overloading by data type

---
+++ over.java
@@ -0,0 +1,21 @@
+class addition
+{
+    static int add(int a, int b)
+    {
+        return a+b;
+    }
+    static double add(double a, double b)
+    {
+        return a+b;
+    }
+}
+ 
+public class over
+ {
+     public static void main(String args[])
+     {
+         System.out.println(addition.add(5,5));
+         System.out.println(addition.add(5,5));
+
+     }
+ }
